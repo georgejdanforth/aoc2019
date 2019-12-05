a79368dd97c61f6a36f1d4fd39d035bea2367f6e
George Danforth <georgedanforth@gmail.com>
2019-12-04 23:42:07 -0500

    day 1

--- src/aoc2019/day_1.clj
+++ src/aoc2019/day_1.clj
@@ -1,5 +1,14 @@
-(ns aoc2019.day-1)
+(ns aoc2019.day-1
+  (:require [aoc2019.utils :refer :all]))
 
-(defn part-1 [] (println "part 1"))
+(defn part-1 []
+  (println
+    (sum (map #(- (quot (parse-int %) 3) 2) (read-lines "data/1-1.txt")))))
 
-(defn part-2 [] (println "part 2"))
+(defn fuel-for-module [mass]
+  (let [fuel-mass (max 0 (- (quot mass 3) 2))]
+    (if (> fuel-mass 0) (+ fuel-mass (fuel-for-module fuel-mass)) 0)))
+
+(defn part-2 []
+  (println
+    (sum (map fuel-for-module (map parse-int (read-lines "data/1-1.txt"))))))
---
+++ src/aoc2019/utils.clj
@@ -0,0 +1,10 @@
+(ns aoc2019.utils
+  (:require [clojure.string :as s]))
+
+(defn read-lines [f] (s/split-lines (slurp f)))
+
+(defn parse-int [string]
+  (try (Integer/parseInt string)
+    (catch Exception exc nil)))
+
+(defn sum [coll] (reduce + coll))
